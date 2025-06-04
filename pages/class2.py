3743612d5bfc32da97e2ee5cd79248b052472137
szxc589974 <szxc589974@gmail.com>
2025-06-04 15:49:20 +0800

    Refactor main.py and add class2.py and tempCodeRunnerFile.py with initial print statements and Streamlit setup

---
+++ pages/class2.py
@@ -0,0 +1,3 @@
+print("test")  # This is a test file
+print()
+print()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title("這是首頁")
-st.write("這是文字")
 print("test")  # This is a test file
 print()
 print()
